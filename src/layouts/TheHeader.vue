c86f4059b1a331e3bec2b09d95613f61d433d6f5
duanribeiro <BJ183r32>
2020-07-29 14:03:57 -0300

    Minor, added tabs

--- src/layouts/TheHeader.vue
+++ src/layouts/TheHeader.vue
@@ -2,27 +2,28 @@
   <v-toolbar
   id="core-toolbar"
   app
-  dark
   style="background: #424242;"
-  flat
   prominent
   >
-    <div class="v-toolbar-title">
-      <v-toolbar-title class="font-weight-light text-general">
-        <v-btn
-        v-if="responsive"
-        class="default v-btn--simple"
-        icon
-        @click.stop="onClickBtn"
-        >
-          <v-icon>mdi-view-list</v-icon>
-        </v-btn>
-        {{ title }}
-      </v-toolbar-title>
-    </div>
-
-    <v-spacer/>
-
+    <v-tabs
+      fixed-tabs
+      background-color="indigo"
+      dark
+    >
+      <v-tab>
+        {{ this.title }}
+      </v-tab>
+      <v-tab>
+        EXEMPLO
+      </v-tab>
+      <v-tab>
+        EXEMPLO
+      </v-tab>
+      <v-tab>
+        EXEMPLO
+      </v-tab>
+      <v-btn @click="sendMessage()">enviar</v-btn>
+    </v-tabs>
   </v-toolbar>
 </template>
 
@@ -35,43 +36,13 @@ export default {
     responsive: false,
     responsiveInput: false
   }),
-
-  computed: {
-    ...mapGetters(['authorized'])
-  },
-
-  watch: {
-    $route (val) {
-      this.title = val.meta.name
-    }
-  },
-
-  mounted () {
-    this.onResponsiveInverted()
-    window.addEventListener('resize', this.onResponsiveInverted)
-  },
-  beforeDestroy () {
-    window.removeEventListener('resize', this.onResponsiveInverted)
-  },
-
   methods: {
-    onClick () {
-      //
-    },
-    onResponsiveInverted () {
-      if (window.innerWidth < 991) {
-        this.responsive = true
-        this.responsiveInput = false
-      } else {
-        this.responsive = false
-        this.responsiveInput = true
-      }
-    },
-    logout: function () {
-      this.$store.dispatch('logout').then(() => {
-        this.$router.push('/')
-      })
+    sendMessage() {
+      this.message = "sended"
     }
+  },
+  created: {
+     axios
   }
 }
 </script>
